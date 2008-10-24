feea4f29f43006b4206c3f4b140f342590169b20
jdorje <jdorje@a0f10bec-cc02-0410-94fc-a9cfff90b4cd>
2008-10-24 18:18:16 +0000

    Delete obsolete branch.

--- branches/GTK1_6/client/connectdlg.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CONNECTDLG_H
-#define __CONNECTDLG_H
-int gui_server_connect(void);
-void connect_dialog_returnkey(Widget w, XEvent *event, String *params,
-			    Cardinal *num_params);
-#endif
--- branches/GTK1_6/gclient/connect.h
+++
@@ -1,4 +0,0 @@
-#ifndef __CONNECTDLG_H
-#define __CONNECTDLG_H
-int gui_server_connect(void);
-#endif
