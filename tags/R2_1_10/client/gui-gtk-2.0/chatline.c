3d1f166b34284fadbf70d97ad6b9dbb1fad61e6e
dmarks <dmarks@a0f10bec-cc02-0410-94fc-a9cfff90b4cd>
2009-11-22 20:01:38 +0000

    tag 2.1.10 release

---
+++ tags/R2_1_10/client/gui-gtk-2.0/chatline.c
@@ -0,0 +1,249 @@
+/********************************************************************** 
+ Freeciv - Copyright (C) 1996 - A Kjeldberg, L Gregersen, P Unold
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+***********************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <gdk/gdkkeysyms.h>
+
+#include "fcintl.h"
+#include "mem.h"
+#include "packets.h"
+#include "support.h"
+
+#include "civclient.h"
+#include "climisc.h"
+#include "clinet.h"
+#include "gui_main.h"
+#include "gui_stuff.h"
+
+#include "chatline.h"
+#include "pages.h"
+
+struct genlist *history_list;
+int history_pos;
+
+/**************************************************************************
+  Helper function to determine if a given client input line is intended as
+  a "plain" public message. Note that messages prefixed with : are a
+  special case (explicit public messages), and will return FALSE.
+**************************************************************************/
+static bool is_plain_public_message(const char *s)
+{
+  /* FIXME: These prefix definitions are duplicated in the server. */
+  const char ALLIES_CHAT_PREFIX = '.';
+  const char SERVER_COMMAND_PREFIX = '/';
+  const char MESSAGE_PREFIX = ':';
+
+  const char *p;
+
+  /* If it is a server command or an explicit ally
+   * message, then it is not a public message. */
+  if (s[0] == SERVER_COMMAND_PREFIX || s[0] == ALLIES_CHAT_PREFIX) {
+    return FALSE;
+  }
+
+  /* It might be a private message of the form
+   *   'player name with spaces':the message
+   * or with ". So skip past the player name part. */
+  if (s[0] == '\'' || s[0] == '"') {
+    p = strchr(s + 1, s[0]);
+  } else {
+    p = s;
+  }
+
+  /* Now we just need to check that it is not a private
+   * message. If we encounter a space then the preceeding
+   * text could not have been a user/player name (the
+   * quote check above eliminated names with spaces) so
+   * it must be a public message. Otherwise if we encounter
+   * the message prefix : then the text parsed up until now
+   * was a player/user name and the line is intended as
+   * a private message (or explicit public message if the
+   * first character is :). */
+  while (p != NULL && *p != '\0') {
+    if (my_isspace(*p)) {
+      return TRUE;
+    } else if (*p == MESSAGE_PREFIX) {
+      return FALSE;
+    }
+    p++;
+  }
+  return TRUE;
+}
+
+
+/**************************************************************************
+...
+**************************************************************************/
+void inputline_return(GtkEntry *w, gpointer data)
+{
+  const char *theinput;
+
+  theinput = gtk_entry_get_text(w);
+  
+  if (*theinput) {
+    if (client_state() == C_S_RUNNING && allied_chat_only
+        && is_plain_public_message(theinput)) {
+      char buf[MAX_LEN_MSG];
+      my_snprintf(buf, sizeof(buf), ". %s", theinput);
+      send_chat(buf);
+    } else {
+      send_chat(theinput);
+    }
+
+    if (genlist_size(history_list) >= MAX_CHATLINE_HISTORY) {
+      void *data;
+
+      data = genlist_get(history_list, -1);
+      genlist_unlink(history_list, data);
+      free(data);
+    }
+
+    genlist_prepend(history_list, mystrdup(theinput));
+    history_pos=-1;
+  }
+
+  gtk_entry_set_text(w, "");
+}
+
+/**************************************************************************
+  Scroll a textview so that the given mark is visible, but only if the
+  scroll window containing the textview is very close to the bottom. The
+  text mark 'scroll_target' should probably be the first character of the
+  last line in the text buffer.
+**************************************************************************/
+static void scroll_if_necessary(GtkTextView *textview,
+                                GtkTextMark *scroll_target)
+{
+  GtkWidget *sw;
+  GtkAdjustment *vadj;
+  gdouble val, max, upper, page_size;
+
+  g_return_if_fail(textview != NULL);
+  g_return_if_fail(scroll_target != NULL);
+
+  sw = gtk_widget_get_parent(GTK_WIDGET(textview));
+  g_return_if_fail(sw != NULL);
+  g_return_if_fail(GTK_IS_SCROLLED_WINDOW(sw));
+
+  vadj = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(sw));
+  val = gtk_adjustment_get_value(GTK_ADJUSTMENT(vadj));
+  g_object_get(G_OBJECT(vadj), "upper", &upper,
+               "page-size", &page_size, NULL);
+  max = upper - page_size;
+  if (max - val < 10.0) {
+    gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(textview), scroll_target,
+                                 0.0, TRUE, 1.0, 0.0);
+  }
+}
+
+/**************************************************************************
+  Appends the string to the chat output window.  The string should be
+  inserted on its own line, although it will have no newline.
+**************************************************************************/
+void real_append_output_window(const char *astring, int conn_id)
+{
+  GtkTextBuffer *buf;
+  GtkTextIter iter;
+  GtkTextMark *mark;
+
+  buf = message_buffer;
+  gtk_text_buffer_get_end_iter(buf, &iter);
+  gtk_text_buffer_insert(buf, &iter, "\n", -1);
+  mark = gtk_text_buffer_create_mark(buf, NULL, &iter, TRUE);
+
+  if (show_chat_message_time) {
+    char timebuf[64];
+    time_t now;
+    struct tm *now_tm;
+
+    now = time(NULL);
+    now_tm = localtime(&now);
+    strftime(timebuf, sizeof(timebuf), "[%H:%M:%S] ", now_tm);
+    gtk_text_buffer_insert(buf, &iter, timebuf, -1);
+  }
+
+  gtk_text_buffer_insert(buf, &iter, astring, -1);
+
+  if (main_message_area) {
+    scroll_if_necessary(GTK_TEXT_VIEW(main_message_area), mark);
+  }
+  if (start_message_area) {
+    scroll_if_necessary(GTK_TEXT_VIEW(start_message_area), mark);
+  }
+  gtk_text_buffer_delete_mark(buf, mark);
+
+  append_network_statusbar(astring, FALSE);
+}
+
+/**************************************************************************
+ I have no idea what module this belongs in -- Syela
+ I've decided to put output_window routines in chatline.c, because
+ the are somewhat related and append_output_window is already here.  --dwp
+**************************************************************************/
+void log_output_window(void)
+{
+  GtkTextIter start, end;
+  gchar *txt;
+
+  gtk_text_buffer_get_bounds(message_buffer, &start, &end);
+  txt = gtk_text_buffer_get_text(message_buffer, &start, &end, TRUE);
+
+  write_chatline_content(txt);
+  g_free(txt);
+}
+
+/**************************************************************************
+...
+**************************************************************************/
+void clear_output_window(void)
+{
+  set_output_window_text(_("Cleared output window."));
+}
+
+/**************************************************************************
+...
+**************************************************************************/
+void set_output_window_text(const char *text)
+{
+  gtk_text_buffer_set_text(message_buffer, text, -1);
+}
+
+/**************************************************************************
+  Scrolls the pregame and in-game chat windows all the way to the bottom.
+**************************************************************************/
+void chatline_scroll_to_bottom(void)
+{
+  GtkTextIter end;
+
+  if (!message_buffer) {
+    return;
+  }
+  gtk_text_buffer_get_end_iter(message_buffer, &end);
+
+  if (main_message_area) {
+    gtk_text_view_scroll_to_iter(GTK_TEXT_VIEW(main_message_area),
+                                 &end, 0.0, TRUE, 1.0, 0.0);
+  }
+  if (start_message_area) {
+    gtk_text_view_scroll_to_iter(GTK_TEXT_VIEW(start_message_area),
+                                 &end, 0.0, TRUE, 1.0, 0.0);
+  }
+}
---
+++ tags/R2_1_10/utility/ftwl/be_common_24.c
@@ -0,0 +1,475 @@
+/**********************************************************************
+ Freeciv - Copyright (C) 2005 - The Freeciv Project
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+***********************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <stdio.h>
+#include <png.h>
+
+#include "fcintl.h"
+#include "log.h"
+#include "mem.h"
+#include "support.h"
+
+#include "back_end.h"
+#include "be_common_24.h"
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#define ENABLE_TRANSPARENCY	1
+#define DO_CYCLE_MEASUREMENTS	0
+
+/* Blend the RGB values of two pixel 3-byte pixel arrays
+ * based on a source alpha value. */
+#define ALPHA_BLEND(d, A, s)          \
+do {                                  \
+  d[0] = (((s[0]-d[0])*(A))>>8)+d[0]; \
+  d[1] = (((s[1]-d[1])*(A))>>8)+d[1]; \
+  d[2] = (((s[2]-d[2])*(A))>>8)+d[2]; \
+} while(0)
+
+/*************************************************************************
+  Combine RGB colour values into a 24bpp colour value.
+*************************************************************************/
+be_color be_get_color(int red, int green, int blue, int alpha)
+{
+  assert(red >= 0 && red <= 255);
+  assert(green >= 0 && green <= 255);
+  assert(blue >= 0 && blue <= 255);
+  assert(alpha >= 0 && alpha <= 255);
+
+  return red << 24 | green << 16 | blue << 8 | alpha;
+}
+
+/*************************************************************************
+  Set a pixel in a given buffer (dest) to a given color (color).
+*************************************************************************/
+static void set_color(be_color color, unsigned char *dest)
+{
+  dest[0] = ((color >> 24) & 0xff);
+  dest[1] = ((color >> 16) & 0xff);
+  dest[2] = ((color >> 8) & 0xff);
+  dest[3] = ((color) & 0xff);
+}
+
+/*************************************************************************
+  Image blit with transparency.  Alpha value lifted from src.
+  MIN_OPACITY means the pixel should not be drawn.  MAX_OPACITY means
+  the pixels should not be blended.
+*************************************************************************/
+/* non static to prevent inlining */
+void image_blit_masked_trans(const struct ct_size *size,
+			     const struct image *src,
+			     const struct ct_point *src_pos,
+			     struct image *dest,
+			     const struct ct_point *dest_pos);
+void image_blit_masked_trans(const struct ct_size *size,
+			     const struct image *src,
+			     const struct ct_point *src_pos,
+			     struct image *dest,
+			     const struct ct_point *dest_pos)
+{
+  int x, y;
+
+  unsigned char *psrc = IMAGE_GET_ADDRESS(src, src_pos->x,
+					  src_pos->y);
+  unsigned char *pdest = IMAGE_GET_ADDRESS(dest, dest_pos->x,
+					   dest_pos->y);
+  int w = size->width;
+  int h = size->height;
+  int extra_src = (src->width - size->width) * 4;
+  int extra_dest = (dest->width - size->width) * 4;
+
+  //printf("BLITTING %dx%d",size->width,size->height);
+
+  for (y = h; y > 0; y--) {
+    for (x = w; x > 0; x--) {
+      unsigned char mask_value = psrc[3];
+
+#if ENABLE_TRANSPARENCY
+      if(mask_value == MAX_OPACITY) {
+	memcpy(pdest, psrc, 4);
+      } else if(mask_value == MIN_OPACITY) {
+	/* 
+	 * Empty since we never copy MAX_OPACITY pixels - that is why
+	 * they exist! 
+	 */
+      } else {
+	/* We need to perform transparency */
+	ALPHA_BLEND(pdest, psrc[3], psrc);
+      }
+#else
+      if (mask_value != MIN_OPACITY) {
+	memcpy(pdest, psrc, 4);
+      }
+#endif
+      psrc += 4;
+      pdest += 4;
+    }
+    psrc += extra_src;
+    pdest += extra_dest;
+  }
+}
+
+/*************************************************************************
+  ...
+*************************************************************************/
+static void clip_two_regions(const struct image *dest,
+			     const struct image *src,
+			     struct ct_size *size,
+			     struct ct_point *dest_pos,
+			     struct ct_point *src_pos)
+{
+  int dx, dy;
+  struct ct_point real_src_pos, real_dest_pos;
+  struct ct_size real_size;
+
+  struct ct_rect src_use =
+      { src_pos->x, src_pos->y, size->width, size->height };
+  struct ct_rect dest_use =
+      { dest_pos->x, dest_pos->y, size->width, size->height };
+
+  real_src_pos = *src_pos;
+  real_dest_pos = *dest_pos;
+
+  ct_clip_rect(&src_use, &src->full_rect);
+  ct_clip_rect(&dest_use, &dest->full_rect);
+
+  real_size.width = MIN(src_use.width, dest_use.width);
+  real_size.height = MIN(src_use.height, dest_use.height);
+
+  dx = MAX(src_use.x - src_pos->x, dest_use.x - dest_pos->x);
+  dy = MAX(src_use.y - src_pos->y, dest_use.y - dest_pos->y);
+
+  real_src_pos.x += dx;
+  real_src_pos.y += dy;
+
+  real_dest_pos.x += dx;
+  real_dest_pos.y += dy;
+
+  *size=real_size;
+  *src_pos=real_src_pos;
+  *dest_pos=real_dest_pos;
+}
+
+/*************************************************************************
+  Blit one image onto another, using transparency value given on all
+  pixels that have alpha mask set.
+*************************************************************************/
+void image_copy(struct image *dest, struct image *src,
+                const struct ct_size *size, const struct ct_point *dest_pos,
+                const struct ct_point *src_pos)
+{
+  struct ct_point real_src_pos = *src_pos, real_dest_pos = *dest_pos;
+  struct ct_size real_size = *size;
+
+  clip_two_regions(dest, src, &real_size, &real_dest_pos, &real_src_pos);
+
+  if (DO_CYCLE_MEASUREMENTS) {
+#define rdtscll(val) __asm__ __volatile__ ("rdtsc" : "=A" (val))
+    unsigned long long start, end;
+    static unsigned long long total_clocks = 0, total_pixels = 0;
+    static int total_blits = 0;
+
+    rdtscll(start);
+
+    image_blit_masked_trans(&real_size, src, &real_src_pos, dest,
+			    &real_dest_pos);
+
+    rdtscll(end);
+    printf("BLITTING %dx%d %dx%d->%dx%d %lld %d\n", real_size.width,
+	   real_size.height, src->width, src->height, dest->width, dest->height,
+	   end - start, real_size.width * real_size.height);
+    total_clocks += (end - start);
+    total_pixels += (real_size.width * real_size.height);
+    total_blits++;
+    if ((total_blits % 1000) == 0) {
+      printf("%f cycles per pixel\n", (float) total_clocks / total_pixels);
+    }
+  } else {
+    image_blit_masked_trans(&real_size, src, &real_src_pos, dest,
+			    &real_dest_pos);
+  }
+}
+
+/*************************************************************************
+  See be_draw_bitmap() below.
+*************************************************************************/
+void draw_mono_bitmap(struct image *image, be_color color,
+                      const struct ct_point *position, 
+                      struct FT_Bitmap_ *bitmap)
+{
+  int x, y;
+  unsigned char *pbitmap = (unsigned char *) bitmap->buffer;
+  unsigned char tmp[4];
+  struct ct_point pos;
+
+  set_color(color, tmp);
+
+  for (y = 0; y < bitmap->rows; y++) {
+    pos.y = y + position->y;
+    for (x = 0; x < bitmap->width; x++) {
+      pos.x = x + position->x;
+      if (ct_point_in_rect(&pos, &image->full_rect)) {
+
+	unsigned char bv = bitmap->buffer[x / 8 + bitmap->pitch * y];
+	if (TEST_BIT(bv, 7 - (x % 8))) {
+	  unsigned char *p =
+	      IMAGE_GET_ADDRESS(image, x + position->x, y + position->y);
+
+	  IMAGE_CHECK(image, x + position->x, y + position->y);
+	  memcpy(p, tmp, 4);
+	}
+      }
+    }
+    pbitmap += bitmap->pitch;
+  }
+}
+
+/*************************************************************************
+  Draw the given bitmap (ie a 1bpp pixmap) in the given color and 
+  position.
+*************************************************************************/
+void draw_alpha_bitmap(struct image *image, be_color color_,
+                       const struct ct_point *position,
+                       struct FT_Bitmap_ *bitmap)
+{
+  int x, y;
+  unsigned char color[4];
+  unsigned char *pbitmap = (unsigned char *) bitmap->buffer;
+  struct ct_point pos;
+
+  set_color(color_, color);
+
+  for (y = 0; y < bitmap->rows; y++) {
+    pos.y = y + position->y;
+    for (x = 0; x < bitmap->width; x++) {
+      pos.x = x + position->x;
+      if (ct_point_in_rect(&pos, &image->full_rect)) {
+	unsigned short transparency = pbitmap[x];
+	unsigned char tmp[4];
+	unsigned char *p =
+	    IMAGE_GET_ADDRESS(image, position->x + x, position->y + y);
+
+	IMAGE_CHECK(image, x, y);
+	ALPHA_BLEND(tmp, transparency, p);
+	memcpy(p, tmp, 4);
+      }
+    }
+    pbitmap += bitmap->pitch;
+  }
+}
+
+/*************************************************************************
+  Set the alpha mask of pixels in a given region of an image.
+*************************************************************************/
+void image_set_alpha(const struct image *image, const struct ct_rect *rect,
+		     unsigned char alpha)
+{
+  int x, y;
+
+  for (y = rect->y; y < rect->y + rect->height; y++) {
+    for (x = rect->x; x < rect->x + rect->width; x++) {
+      IMAGE_CHECK(image, x, y);
+      IMAGE_GET_ADDRESS(image, x, y)[3] = alpha;
+    }
+  }
+}
+
+/*************************************************************************
+  Perform 
+     dest_alpha = (dest_alpha * src_alpha)/256
+*************************************************************************/
+void image_multiply_alphas(struct image *dest, const struct image *src,
+			   const struct ct_point *src_pos)
+{
+  struct ct_point real_src_pos = *src_pos, real_dest_pos = { 0, 0 };
+  struct ct_size real_size = { dest->width, dest->height };
+
+  clip_two_regions(dest, src, &real_size, &real_dest_pos, &real_src_pos);
+  {
+    int x, y;
+
+    for (y = 0; y < real_size.height; y++) {
+      for (x = 0; x < real_size.width; x++) {
+	unsigned char *psrc = IMAGE_GET_ADDRESS(src, x + real_src_pos.x,
+						y + real_src_pos.y);
+	unsigned char *pdest =
+	    IMAGE_GET_ADDRESS(dest, x + real_dest_pos.x,
+			      y + real_dest_pos.y);
+
+	IMAGE_CHECK(src, x + real_src_pos.x, y + real_src_pos.y);
+	IMAGE_CHECK(dest, x + real_dest_pos.x, y + real_dest_pos.y);
+	pdest[3] = (psrc[3] * pdest[3]) / 256;
+      }
+    }
+  }
+}
+
+/*************************************************************************
+  Copy image buffer src to dest without doing any alpha-blending.
+*************************************************************************/
+void image_copy_full(struct image *src, struct image *dest,
+		     struct ct_rect *region)
+{
+  int y;
+
+  for (y = 0; y < region->height; y++) {
+    unsigned char *psrc = IMAGE_GET_ADDRESS(src, region->x, 
+					    y + region->y);
+    unsigned char *pdest = IMAGE_GET_ADDRESS(dest, 0, y);
+
+    IMAGE_CHECK(src, region->x, y + region->y);
+    IMAGE_CHECK(src, region->x + region->width + -1, y + region->y);
+    IMAGE_CHECK(dest, 0, y);
+    IMAGE_CHECK(dest, region->width - 1, y);
+
+    memcpy(pdest, psrc, 4 * region->width);
+  }
+}
+
+/*************************************************************************
+  Allocate and initialize an image struct.
+*************************************************************************/
+struct image *image_create(int width, int height)
+{
+  struct image *result = fc_malloc(sizeof(*result));
+
+  result->pitch = width * 4;
+  result->data = fc_malloc(result->pitch * height);
+  result->width = width;
+  result->height = height;
+  result->full_rect.x = 0;
+  result->full_rect.y = 0;
+  result->full_rect.width = width;
+  result->full_rect.height = height;
+
+  return result;
+}
+
+/*************************************************************************
+  Free an image struct.
+*************************************************************************/
+void image_destroy(struct image *image)
+{
+  free(image->data);
+  free(image);
+}
+
+/*************************************************************************
+  ...
+*************************************************************************/
+struct image *image_load_gfxfile(const char *filename)
+{
+  png_structp pngp;
+  png_infop infop;
+  png_int_32 width, height, x, y;
+  FILE *fp;
+  struct image *xi;
+
+  fp = fopen(filename, "rb");
+  if (!fp) {
+    freelog(LOG_FATAL, _("Failed reading PNG file: %s"), filename);
+    exit(EXIT_FAILURE);
+  }
+
+  pngp = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!pngp) {
+    freelog(LOG_FATAL, _("Failed creating PNG struct"));
+    exit(EXIT_FAILURE);
+  }
+
+  infop = png_create_info_struct(pngp);
+  if (!infop) {
+    freelog(LOG_FATAL, _("Failed creating PNG struct"));
+    exit(EXIT_FAILURE);
+  }
+  
+  if (setjmp(pngp->jmpbuf)) {
+    freelog(LOG_FATAL, _("Failed while reading PNG file: %s"), filename);
+    exit(EXIT_FAILURE);
+  }
+
+  png_init_io(pngp, fp);
+  png_read_info(pngp, infop);
+
+  width = png_get_image_width(pngp, infop);
+  height = png_get_image_height(pngp, infop);
+
+  freelog(LOG_DEBUG, "reading '%s' (%ldx%ld) bit depth=%d color_type=%d",
+	  filename, width, height, png_get_bit_depth(pngp, infop),
+	  png_get_color_type(pngp, infop));
+
+  if (png_get_color_type(pngp,infop) == PNG_COLOR_TYPE_PALETTE) {
+    png_set_palette_to_rgb(pngp);
+  }
+
+  if (png_get_valid(pngp, infop, PNG_INFO_tRNS)) {
+    png_set_tRNS_to_alpha(pngp);
+  }
+
+  if (png_get_bit_depth(pngp,infop) == 16) {
+    png_set_strip_16(pngp);
+  }
+
+  if (png_get_bit_depth(pngp,infop) < 8) {
+    png_set_packing(pngp);
+  }
+
+  /* Add an alpha channel for RGB images */
+  if ((png_get_color_type(pngp, infop) & PNG_COLOR_MASK_ALPHA) == 0) {
+    png_set_filler(pngp, 0xff, PNG_FILLER_AFTER);
+  }
+
+  xi = image_create(width, height);
+
+  png_read_update_info(pngp, infop);
+
+  {
+    png_bytep pb;
+    png_uint_32 stride = png_get_rowbytes(pngp, infop);
+    png_bytep buf = fc_malloc(stride * height);
+    png_bytep *row_pointers = fc_malloc(height * sizeof(png_bytep));
+
+    assert(stride >= width * 4);
+
+    for (y = 0, pb = buf; y < height; y++, pb += stride) {
+      row_pointers[y] = pb;
+    }
+
+    png_read_image(pngp, row_pointers);
+    png_read_end(pngp, infop);
+    free(row_pointers);
+    png_destroy_read_struct(&pngp, &infop, NULL);
+    fclose(fp);
+
+    pb = buf;
+
+    for (y = 0; y < height; y++) {
+      for (x = 0; x < width; x++) {
+	png_bytep src = pb + 4 * x;
+	unsigned char *dest = IMAGE_GET_ADDRESS(xi, x, y);
+
+        memcpy(dest, src, 4);
+      }
+      pb += stride;
+    }
+    free(buf);
+  }
+
+  return xi;
+}
